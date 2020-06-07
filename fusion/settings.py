bd2b534cd9239dba769102815b7339625bc91988
darricson <darricson2@gmail.com>
2020-06-07 20:33:12 -0300

    Modificando o debug para false nos setitigns para o deploy

--- fusion/settings.py
+++ fusion/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(tgscn)fxy%&hwx69_kb+c$+m=l3#oba+*$(-#^^2bfo@-26sa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
