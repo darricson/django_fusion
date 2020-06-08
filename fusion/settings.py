b162388dd72aaea166c236544705d25bd121282b
darricson <darricson2@gmail.com>
2020-06-07 21:20:51 -0300

    Acrescentado caminho do staticfiles no settings

--- fusion/settings.py
+++ fusion/settings.py
@@ -127,9 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #  configuração de e-mail local para teste no console
